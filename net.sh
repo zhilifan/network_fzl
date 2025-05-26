f2fe93931196ac9f52c7153c64408dc1348c6659
zhilifan <91236342+zhilifan@users.noreply.github.com>
2025-05-26 14:37:57 +0800

    Update net.sh

--- net.sh
+++ net.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 counter=1
-for bri in $(seq 1 20); do
+sta=1
+end=20
+for bri in $(seq $sta $end); do
     echo "interface bri $bri"
     echo "link-a mode d"
     echo " quit"
     ((counter++))
 done
+
+echo "int ran "
