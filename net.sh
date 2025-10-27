ba90be9e6990494b1d7ca6e57d9ac8155d9be807
zhilifan <91236342+zhilifan@users.noreply.github.com>
2025-10-27 14:38:44 +0800

    Update net.sh

--- net.sh
+++ net.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 counter=1
 sta=1
-end=42
+end=41
 echo "shell" > shell.txt
 for bri in $(seq $sta $end); do
     echo "interface bri $bri"
@@ -25,4 +25,23 @@ port trunk permit vlan 200 290 300 390 400 to 401 600 700
 link-aggregation mode dynamic
 stp root-protection
 EOF
-echo "test config" >>test.cfg
+cat <<EOF
+vlan 200
+ description Office-Wifi
+vlan 300
+ description Office-Wired
+vlan 390
+ description Office-Guest&Fail
+vlan 400
+ description Network Management
+vlan 401
+ description AP Management
+vlan 600
+ description PD-Wifi
+vlan 700
+ description PD-Wired
+vlan 901
+des jk-nvr
+vlan 910
+des jk-Camera
+EOF
